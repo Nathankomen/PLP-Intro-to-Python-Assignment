40dcdd654402124c07ac3fdf31e833da21f7ce17
Nathan Komen <117839384+Nathankomen@users.noreply.github.com>
2024-08-13 20:21:18 +0300

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# To Ask the user for their name
+name = input("What is your name? ")
+
+# To Ask the user for their age
+age = input("How old are you? ")
+
+# To Ask the user for their location
+location = input("Where do you live? ")
+
+# To Print out a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
